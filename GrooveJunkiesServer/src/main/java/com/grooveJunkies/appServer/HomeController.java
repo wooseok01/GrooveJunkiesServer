f35deaa93e2bbe10c427161b34de46c723b1ca61
Wooseok Song <bboyskip115@gmail.com>
2017-03-05 00:52:59 +0900

    1. make its setting. -> mvc, son, multipart-form data recieved

---
+++ GrooveJunkiesServer/src/main/java/com/grooveJunkies/appServer/HomeController.java
@@ -0,0 +1,52 @@
+package com.grooveJunkies.appServer;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value="/register", method = RequestMethod.POST)
+	public String register(Model model, 
+			@RequestParam(value="email", required=false) String email,
+			@RequestParam(value="password", required=false) String password,
+			@RequestParam(value="stageName", required=false) String stageName,
+			@RequestParam(value="thumnail", required=false) MultipartFile photo){
+		System.out.println("im in!");
+		
+		return null;
+	}
+	
+}
